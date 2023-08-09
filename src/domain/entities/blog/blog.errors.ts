09fa263ad0df8549b949bf96217507060f8be333
muzzamilr <mmuzzamil90@gmail.com>
2023-08-09 15:09:37 +0500

    remove blog entity and repository

--- src/domain/entities/blog/blog.errors.ts
+++
@@ -1,5 +0,0 @@
-import { AlreadyExistsError, NotFoundError } from "../base/base.errors";
-
-export class BlogNotFound extends NotFoundError {}
-
-export class BlogAlreadyExists extends AlreadyExistsError {}
--- src/domain/entities/blog/blog.repository.ts
+++
@@ -1,4 +0,0 @@
-import { BaseRepository } from "../base/base.repository";
-import { BlogEntity } from "./blog.entity";
-
-export abstract class BlogRepository extends BaseRepository<BlogEntity> {}
